7fdd6d5b32efc213cdecade583743265442176fa
Fran Agulto <francis.agulto@wpengine.com>
2024-04-05 10:37:45 -0500

    added loading reactivity to index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,11 @@
 <script>
   import { onMount } from 'svelte';
-
+  
   let posts = [];
+  let loading = true;
 
   async function fetchGraphQL(query = {}) {
-    // Encode the GraphQL query as a URL parameter for GET requests
-    const queryParams = new URLSearchParams({
-      query: query,
-     
-    }).toString();
-
+    const queryParams = new URLSearchParams({ query }).toString();
     const response = await fetch(`${import.meta.env.VITE_GRAPHQL_ENDPOINT}?${queryParams}`, {
       method: 'GET',
       headers: {
@@ -20,7 +16,6 @@
     if (!response.ok) {
       throw new Error(`Network error: ${response.statusText}`);
     }
-
     return response.json();
   }
 
@@ -41,33 +36,37 @@
   onMount(async () => {
     try {
       const { data, errors } = await fetchGraphQL(GET_POSTS_QUERY);
-
       if (errors) {
         console.error('Errors returned from server:', errors);
-        return;
+      } else {
+        posts = data.posts.nodes;
       }
-
-      posts = data.posts.nodes;
     } catch (error) {
       console.error('An error occurred:', error);
+    } finally {
+      loading = false;
     }
   });
 </script>
 
-<div class="bg-custom-dark min-h-screen text-hot-pink p-8">
-  <h1 class="text-4xl mb-8 font-bold text-center">Fran's Headless WP with SvelteKit Blog Example</h1>
-  <ul class="space-y-4">
-    {#each posts as post}
-    <li class="text-center">
-      <a href={`/blog${post.uri}`} class="text-2xl hover:text-pink-600">{post.title}</a>
-      {#if post.date}
-        <p class="text-gray-500 text-sm">{new Date(post.date).toLocaleDateString()}</p>
-      {:else}
-        <p class="text-gray-500 text-sm">No date available</p>
-      {/if}
-    </li>
-  {/each}
-  
-  </ul>
-</div>
-
+{#if loading}
+  <div class="flex justify-center items-center min-h-screen">
+    <p>Loading...</p>
+  </div>
+{:else}
+  <div class="bg-custom-dark min-h-screen text-hot-pink p-8">
+    <h1 class="text-4xl mb-8 font-bold text-center">Fran's Headless WP with SvelteKit Blog Example</h1>
+    <ul class="space-y-4">
+      {#each posts as post}
+        <li class="text-center">
+          <a href={`/blog${post.uri}`} class="text-2xl hover:text-pink-600">{post.title}</a>
+          {#if post.date}
+            <p class="text-gray-500 text-sm">{new Date(post.date).toLocaleDateString()}</p>
+          {:else}
+            <p class="text-gray-500 text-sm">No date available</p>
+          {/if}
+        </li>
+      {/each}
+    </ul>
+  </div>
+{/if}
